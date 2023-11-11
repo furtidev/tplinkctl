1fef2cc3be4709bb94570e78f70e7a60aaccbc5c
furtidev <midrashahil123@gmail.com>
2023-11-11 21:23:25 +0600

    move wr840n specific functions to it's own package

--- utils/utils.go
+++ utils/utils.go
@@ -1,37 +1,10 @@
 package utils
 
 import (
-	"net/http"
-	"io"
-	"strings"
 	"encoding/base64"
 )
 
 func EncodeBase64(in string) (out string) {
 	out = base64.StdEncoding.EncodeToString([]byte(in))
 	return
-}
-
-func MakeRequest(url string, payload string, auth string) (string, error) {
-	req, err := http.NewRequest("POST", url, strings.NewReader(payload))
-	if err != nil {
-		return "", err
-	}
-	req.Header.Add("Referer", "http://192.168.0.1/mainFrame.htm")
-	req.Header.Add("Content-Type", "text/plain")
-	req.Header.Add("charset", "UTF-8")
-	req.Header.Add("Cookie", "Authorization="+auth)
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(body), nil
 }
